f1e2dcb417170c875f5645ff11bcfcdb05f82fee
Corrado Cavalli <corradocavalli@hotmail.com>
2018-03-20 07:33:30 +0100

    Add project files.

---
+++ ShareTarget/MyVm.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MyVm.cs" company="IBV Informatik AG">
+//    Copyright (c) IBV Informatik AG All rights reserved.
+// </copyright>
+// 
+//  author: Corrado Cavalli
+// created: 2018-03-19 @ 21:48
+//  edited: 2018-03-19 @ 21:53
+// -----------------------------------------------------------------------
+
+#region Using
+
+using System.Collections.ObjectModel;
+
+#endregion
+
+namespace ShareTarget
+{
+    public class MyVm
+    {
+        private static MyVm instance;
+        public static MyVm Instance => instance ?? (instance = new MyVm());
+
+        public MyVm()
+        {
+            this.Values.Add(new MyUnit());
+            instance = this;
+        }
+
+        public ObservableCollection<MyUnit> Values { get; set; }=new ObservableCollection<MyUnit>();
+    }
+
+    public class MyUnit
+    {
+        public string Value { get; set; }
+    }
+}
---
+++ ShareTarget/ShareView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ShareTarget
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShareView : Page
+    {
+        public ShareView()
+        {
+            this.InitializeComponent();
+            this.Loaded += async (s, e) =>
+            {
+                ShareViewViewModel.Operation.ReportStarted();
+
+                //TAKE 1 :-(
+                //MyVm.Instance.Values.Add(new MyUnit());
+
+                //TAKE2 :-(
+                //await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                //{
+                //    MyVm.Instance.Values.Add(new MyUnit());
+                //});
+
+                //TAKE3 :-)
+                await Task.Run((async () =>
+                {
+                    await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            
+                            MyVm.Instance.Values.Add(new MyUnit());
+                        });
+                }));
+
+                ShareViewViewModel.Operation.ReportCompleted();
+            };
+        }
+    }
+}
